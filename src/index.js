7140b502959d425a03b220eb4d8e731d4c68df84
Ode42 <ode.progr@outlook.com>
2020-12-02 15:45:27 +0200

    Install Nodemon and add dev script

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ app.get('/', (req, res) => {
 res.send("Hello from Klapi")
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 app.listen(port, () => {
 console.log(`Server running on port ${port}`)
 })
