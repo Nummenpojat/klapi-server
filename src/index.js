7b0b8aac4e0bf40e7051432d64b87d02a265f19a
Ode42 <ode.progr@outlook.com>
2020-12-02 18:21:39 +0200

    Add API endpoint for getting user

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express()
 
+const user = {
+    firstName: "John",
+    lastName: "Doe",
+    localGroup: "Hyvinkään Nummenpojat",
+    ageSection: "Explorer",
+    positions: []
+  }
+
 app.get('/', (req, res) => {
 res.send("Hello from Klapi")
+})
+
+app.get('/api/user', (req, res) => {
+    res.send(user)
 })
 
 const port = process.env.PORT || 3001
