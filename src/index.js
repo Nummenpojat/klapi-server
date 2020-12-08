cc2ca2063dc8f7c39acbad013cfe09257ad15a44
Ode42 <ode.progr@outlook.com>
2020-12-08 20:24:50 +0200

    Add users object and tasks API

--- src/index.js
+++ src/index.js
@@ -4,20 +4,49 @@ var cors = require('cors')
 
 app.use(cors()) // cors middleware
 
-const user = {
-    firstName: "John",
-    lastName: "Doe",
-    localGroup: "Hyvinkään Nummenpojat",
-    ageSection: "Explorer",
-    positions: []
+const users = {
+    john_doe: {
+        info: {
+            id: 0,
+            firstName: "John",
+            lastName: "Doe",
+            localGroup: "Hyvinkään Nummenpojat",
+            ageSection: "Explorer",
+            roles: ["small group leader", "marketing leader"]
+        },
+        tasks: [
+            {
+                name: "Dance in stage",
+                points: 10,
+                completed: false
+            },
+            {
+                name: "Take a picture",
+                points: 10,
+                completed: false
+            },
+            {
+                name: "Meet camp leader",
+                points: 15,
+                completed: true
+            }
+        ]
+    }
   }
+
 
 app.get('/', (req, res) => {
 res.send("Hello from Klapi")
 })
 
-app.get('/api/user', (req, res) => {
-    res.send(user)
+app.get('/api/users/:userId', (req, res) => {
+    let user = req.params.userId
+    res.send(users[user].info)
+})
+
+app.get('/api/users/:userId/tasks', (req, res) => {
+    let user = req.params.userId
+    res.send(users[user].tasks)
 })
 
 const port = process.env.PORT || 3001
