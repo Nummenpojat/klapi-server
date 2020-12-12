9624acc58e15658cb3bfa5081067dbcb10d3dcfd
Ode42 <ode.progr@outlook.com>
2020-12-12 21:18:08 +0200

    Add users-object

--- src/index.js
+++ src/index.js
@@ -13,24 +13,17 @@ const users = {
             localGroup: "Hyvinkään Nummenpojat",
             ageSection: "Explorer",
             roles: ["small group leader", "marketing leader"]
-        },
-        tasks: [
-            {
-                name: "Dance in stage",
-                points: 10,
-                completed: false
-            },
-            {
-                name: "Take a picture",
-                points: 10,
-                completed: false
-            },
-            {
-                name: "Meet camp leader",
-                points: 15,
-                completed: true
-            }
-        ]
+        }
+    },
+    testaaja: {
+        info: {
+            id: 1,
+            firstName: "Testaaja",
+            lastName: "Testaaja",
+            localGroup: "Hyvinkään Nummenpojat",
+            ageSection: "Rover",
+            roles: []
+        }
     }
   }
 
@@ -39,15 +32,7 @@ app.get('/', (req, res) => {
 res.send("Hello from Klapi")
 })
 
-app.get('/api/users/:userId', (req, res) => {
-    let user = req.params.userId
-    res.send(users[user].info)
-})
 
-app.get('/api/users/:userId/tasks', (req, res) => {
-    let user = req.params.userId
-    res.send(users[user].tasks)
-})
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
